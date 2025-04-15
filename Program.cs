19c024a87719de525ff368559d4449495fb25419
Badar987 <hassanbadar506@gmail.com>
2025-04-15 23:54:24 +0500

    Add Jwt Configration and Run Migration and update DataBase with the new Tables thats are created becase of Iddenty User

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using GennyOnlineStoreBE.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,8 +17,11 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+//Identity Configration
 builder.Services.AddIdentity<ApplicationUsers, IdentityRole>(options =>
 {
+    //These configration just for testing purpose becase we dont want to add password again and again
     options.Password.RequireDigit = false;
     options.Password.RequireLowercase = false;
     options.Password.RequireUppercase = false;
@@ -27,6 +32,27 @@ builder.Services.AddIdentity<ApplicationUsers, IdentityRole>(options =>
 
 }).AddEntityFrameworkStores<AppDbContext>()  //Save these configration these required for Identity Role Based Authentication
 .AddDefaultTokenProviders(); //Add this method becase we want to use jwt token 
+
+//Jwt Configration
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = false,
+        ValidIssuer = builder.Configuration["Jwt: Issuer"],
+        ValidAudience = builder.Configuration["Jwt: Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt: Key"]))
+    };
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
